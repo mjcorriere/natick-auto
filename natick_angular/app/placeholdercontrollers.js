67c6460351a43597ead388530de2e8ed14867c06
mjc <markjcorriere@gmail.com>
2014-09-30 17:27:06 -0400

    More prepwork. Complete test routes back to job overview screen.

--- natick_angular/app/placeholdercontrollers.js
+++ natick_angular/app/placeholdercontrollers.js
@@ -17,17 +17,17 @@ natickModule.controller('joboverviewController',
   ['$scope', '$routeParams', 'MockRetrievalService', 
   function($scope, $routeParams, RetrievalService) {
 
-    $scope.jobid = $routeParams.jobid;
+    $scope.jobid          = $routeParams.jobid;
 
-    $scope.customer     = RetrievalService.getCustomer($scope.jobid);
-    $scope.pendingTests = RetrievalService.getPendingTests($scope.jobid);
+    $scope.customer       = RetrievalService.getCustomer($scope.jobid);
+    $scope.pendingTests   = RetrievalService.getPendingTests($scope.jobid);
     $scope.completedTests = RetrievalService.getCompletedTests($scope.jobid);
 
 }]);
 
 natickModule.controller('breakstrengthController', 
-  ['$scope', '$routeParams', 'MockRetrievalService', 'BreakStrengthService',
-  function($scope, $routeParams, RetrievalService, BreakStrengthService) {
+  ['$scope', '$location', '$routeParams', 'MockRetrievalService', 'BreakStrengthService',
+  function($scope, $location, $routeParams, RetrievalService, BreakStrengthService) {
 
     $scope.jobid            = $routeParams.jobid;
 
@@ -35,8 +35,8 @@ natickModule.controller('breakstrengthController',
     $scope.spec             = 'Mil-C-1001';
     $scope.testMethod       = 'ASTM-D-5034';
 
-    $scope.warpTest         = BreakStrengthService.warpTest();
-    $scope.fillTest         = BreakStrengthService.fillTest();
+    $scope.warpTest         = BreakStrengthService.warpTest($scope.jobid);
+    $scope.fillTest         = BreakStrengthService.fillTest($scope.jobid);
 
     $scope.addWarpSample    = BreakStrengthService.addWarpSample;
     $scope.removeWarpSample = BreakStrengthService.removeWarpSample;
@@ -44,26 +44,32 @@ natickModule.controller('breakstrengthController',
     $scope.addFillSample    = BreakStrengthService.addFillSample;
     $scope.removeFillSample = BreakStrengthService.removeFillSample;
 
-    $scope.completeTest     = BreakStrengthService.completeTest;
+    $scope.completeTest     = function() {
+      BreakStrengthService.completeTest();
+      $location.path('job/' + $scope.jobid);
+    }
 
     $scope.$on('$routeChangeStart', function(event, next, current) {
       BreakStrengthService.saveData();
     });
 
-
-
 }]);
 
 natickModule.controller('visualshadeController', 
-  ['$scope', 'VisualShadeService',
-  function($scope, VisualShadeService) {
+  ['$scope', '$location', '$routeParams', 'VisualShadeService',
+  function($scope, $location, $routeParams, VisualShadeService) {
+
+    $scope.jobid        = $routeParams.jobid;
 
     $scope.testData     = VisualShadeService.testData();
     
     $scope.removeSample = VisualShadeService.removeSample;
     $scope.addSample    = VisualShadeService.addSample;
 
-    $scope.completeTest = VisualShadeService.completeTest;
+    $scope.completeTest = function() {
+      VisualShadeService.completeTest();
+      $location.path('job/' + $scope.jobid);
+    }
 
     $scope.$on('$routeChangeStart', function(event, next, current) {
       VisualShadeService.saveData();
